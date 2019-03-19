214ced7ddf55f2a188e67ae87675f4da5c0de40b
Dylan Klingensmith <43608302+ahs-DKlingensmith@users.noreply.github.com>
2019-03-19 08:32:54 -0400

    Add files via upload

---
+++ Activity 6 Questions.java
@@ -0,0 +1,15 @@
+Dylan Klingensmith
+Period 2
+3/19/19
+Activity 6 Questions
+
+1. Remove the 6 of clubs and 5 of spades
+   Remove the 6 of clubs and 5 of clubs
+  
+2. The only way to win is if the last three cards are a jack, queen, and king. The last three cards do not have to be those three, but 
+   the player loses otherwise.
+   
+3. The random nature of the game leaves little room for skill and strategy. It does not necessarily matter the order in which pairs and triplets are removed,
+   but rather in the next two or three cards placed face-up from the deck.
+
+   
